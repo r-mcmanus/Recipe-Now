2aef3982f76add7b599b7a26aa295a1843ca7dd1
rmcmanus <rmcmanus@augusta.edu>
2020-10-28 17:59:13 -0400

    Add project files.

---
+++ Recipe Now/Pages/Likes.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Recipe_Now.Pages
+{
+    public class LikesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
