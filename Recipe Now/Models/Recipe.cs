750051c76e80672c5b390e77736c3558ba746b10
rmcmanus <rmcmanus@augusta.edu>
2020-11-10 17:05:25 -0500

    Bobby tries to merge his stuff into master

---
+++ Recipe Now/Models/Recipe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipe_Now.Models
+{
+    public class Recipe
+    {
+        public string Name  { get; set; }
+
+        public string URL { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public List<string> Ingredients { get; set; } = new List<string>();
+    }
+}
